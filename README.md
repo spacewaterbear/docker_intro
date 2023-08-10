96ad10420e8629d0b844e5a0a213825f40faaed0
spacewaterbear <emmanuel.thiery.pro@gmail.com>
2023-08-10 18:54:52 +0900

    [ADD] base

--- README.md
+++ README.md
@@ -1,14 +1,33 @@
 # Initiation Docker 
 
 ## Objectifs
-- comprendre rapidement la containerization
+- comprendre rapidement la containerization : https://algodaily.com/lessons/what-is-a-container-a-docker-tutorial
 - utilisation de docker-compose
 - récupérer une image docker depuis docker hup
 - créer une image docker, et la pusher sur dockerhub
 - mettre une API sur docker, et l'utiliser en local
 
+## Prérequis
+- avoir installer docker et docker compose
+pour vérifier : 
+```bash
+docker -v
+docker-compose -v
+```
+run the hello world image
+```bash
+docker run hello-world
+```
 ## Vocabulaire
 - image
 - contenaire
 - volume
 - exposition port
+- architecture
+
+## Starting
+
+1. Run code in local without docker
+```bash
+uvicorn main:app --reload
+```
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+app = FastAPI()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.get("/")
+def read_root():
+    return RedirectResponse(url="/docs")
+
+
+@app.get("/bonjour")
+def read_root():
+    return {"message": "Bonjour, le !"}
+
+
